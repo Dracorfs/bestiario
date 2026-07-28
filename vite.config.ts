0428af8e3dfc290f21f2aead65666dfe0b721bc8
Dracorfs <franciscosantopietro@hotmail.com>
2026-07-27 23:01:22 -0300

    fix dependencies and build order

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
-    tailwindcss(),
     tanstackStart({
       tsr: {
         appDirectory: "src",
       },
     }),
+    react(),
+    tailwindcss(),
   ],
 });
